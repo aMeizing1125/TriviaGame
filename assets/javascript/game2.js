d8ec9672fa62b53f83badff241adda6aa730ad3e
unknown <hampton.muriel@gmail.com>
2019-02-16 21:46:50 -0500

    Trying to get the answers to populate. It worked okay with the using jquery to .text into th div. but then I changed it to input.. didn't work then added label for the input.. still nothing

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -7,15 +7,14 @@
 //modal windows - extra (optional) show 2 divs and based on which one is clicked
 //timer
 
-window.onload = function() {
+window.onload = function () {
   $(".reset").on("click", stopwatch.reset);
   $(".start").on("click", stopwatch.start);
 };
 
 
 
-questionsArray = [
-  {
+questionsArray = [{
     question: "What does HTML mean?",
     answers: ["HyperText Markup Language", "Hypertech Markup Language", "Holly Toledo Make Loli", "Hyper Text Markup Language"],
     correctAnswer: "HyperText Markup Language",
@@ -27,7 +26,7 @@ questionsArray = [
   },
   {
     question: "What does API stand represent?",
-    answer: ["Anti-Pornography Initiative", "Application Programming Interface", "Application Properties Interface", "Applicaiton Proxy Interface"],
+    answers: ["Anti-Pornography Initiative", "Application Programming Interface", "Application Properties Interface", "Applicaiton Proxy Interface"],
     correctAnswer: "Application Programming Interface",
   }
 ];
@@ -40,67 +39,71 @@ var clockRunning = false;
 
 //game var's
 var playersGuess;
-
+//stuff hide() before game start 
 $('#submit').hide();
+$('.answer1').hide();
+$('.answer2').hide();
+$('.answer3').hide();
+$('.answer4').hide();
+
 // Our stopwatch object
 
 var stopwatch = {
-//change this back to 30secs after testing***
+  //change this back to 30secs after testing***
   // time: 30,
   time: 5,
   // lap: 1,
 
-  reset: function() {
+  reset: function () {
     stopwatch.time = 1000 * 30;
     $(".timer").text("Timer: 00:30");
+    //change to 30
+    this.time = 5;
+    $('#submit').hide();
   },
 
-  start: function() {
+  start: function () {
     if (!clockRunning) {
       //the reason this isn't a -1000 is because it has to count up like normal time you can't make it go backwards yet. 
       intervalId = setInterval(stopwatch.count, 1000);
       clockRunning = true;
       $('#submit').show();
-      nextQuestion();
+      $('.questionArea').text(questionsArray[0].question);
+      //the answers aren't displaying 
+      $('.answer1').text(questionsArray[0].answers[0]);
+      $('.answer2').text(questionsArray[0].answers[1]);
+      $('.answer3').text(questionsArray[0].answers[2]);
+      $('.answer4').text(questionsArray[0].answers[3]);
     }
   },
-  stop: function() {
+  nextQuestion: function() {
+
+  },
+  stop: function () {
 
     // DONE: Use clearInterval to stop the count here and set the clock to not be running.
     clearInterval(intervalId);
     clockRunning = false;
   },
- 
-  count: function() {
-    stopwatch.time--;
 
+  count: function () {
+    stopwatch.time--;
     var converted = stopwatch.timeConverter(stopwatch.time);
 
     // DONE: Use the variable we just created to show the converted time in the "display" div.
     $(".timer").text(converted);
+
     //this prevents the countdown going into the negative numbers. 
-    if (stopwatch.time === 0 ) {
+    if (stopwatch.time === 0) {
       // you have to do stopwatch.stop() is because object within a function. 
       stopwatch.stop();
       console.log("yes it works below 0")
     }
   },
-//so I moved this from underneath the count()  so it would be separate and I could call it. 
-//for some reason it seemed smarter than leaving it within the count() 
-  nextQuestion: function() {
+  //so I moved this from underneath the count()  so it would be separate and I could call it. 
+  //for some reason it seemed smarter than leaving it within the count() 
 
-    $('.questionArea').text(questionsArray[0].question);
-    //the answers aren't displaying 
-    $('.answer1').text(questionsArray[0].answer[0]);
-    $('.answer2').text(questionsArray[0].answer[1]);
-    $('.answer3').text(questionsArray.answer[2]);
-    $('.answer4').text(questionsArray.answer[3]);
-
-    
-      // you need to record the users choice and compare
-  },
-  
-  timeConverter: function(t) {
+  timeConverter: function (t) {
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
 
@@ -110,11 +113,9 @@ var stopwatch = {
 
     if (minutes === 0) {
       minutes = "00";
-    }
-    else if (minutes < 10) {
+    } else if (minutes < 10) {
       // minutes = "0" + minutes;
     }
-
     return minutes + ":" + seconds;
   }
 };
