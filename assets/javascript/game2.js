bfcdaedeada067d83c8cd974dcd3802830e934d8
unknown <hampton.muriel@gmail.com>
2019-02-16 20:46:46 -0500

    why won't these damn answer's display properlygit add .git add .! arghhh

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,6 +1,8 @@
 //Questions => array of obj
 // questions = [{},{},{}]
 //questions need an image
+//Yes, I did steal the example. I assumed that's what their  intended purpose was. 
+//Why reinvent the wheel when I can just take this one. I did retype it multiple times
 
 //modal windows - extra (optional) show 2 divs and based on which one is clicked
 //timer
@@ -10,17 +12,42 @@ window.onload = function() {
   $(".start").on("click", stopwatch.start);
 };
 
+
+
+questionsArray = [
+  {
+    question: "What does HTML mean?",
+    answers: ["HyperText Markup Language", "Hypertech Markup Language", "Holly Toledo Make Loli", "Hyper Text Markup Language"],
+    correctAnswer: "HyperText Markup Language",
+  },
+  {
+    question: "What symbol notates a class in CSS?",
+    answers: ["@", "#", "%", "."],
+    correctAnswer: ".",
+  },
+  {
+    question: "What does API stand represent?",
+    answer: ["Anti-Pornography Initiative", "Application Programming Interface", "Application Properties Interface", "Applicaiton Proxy Interface"],
+    correctAnswer: "Application Programming Interface",
+  }
+];
 //  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
 
 // prevents the clock from being sped up unnecessarily
 var clockRunning = false;
 
+
+//game var's
+var playersGuess;
+
+$('#submit').hide();
 // Our stopwatch object
 
 var stopwatch = {
-
-  time: 30,
+//change this back to 30secs after testing***
+  // time: 30,
+  time: 5,
   // lap: 1,
 
   reset: function() {
@@ -33,6 +60,17 @@ var stopwatch = {
       //the reason this isn't a -1000 is because it has to count up like normal time you can't make ti go backwards yet. 
       intervalId = setInterval(stopwatch.count, 1000);
       clockRunning = true;
+      $('#submit').show();
+      $('.questionArea').text(questionsArray[0].question);
+      //the answers aren't displaying 
+      $('.answer1').text(questionsArray[0].answer[0]);
+      $('.answer2').text(questionsArray[0].answer[1]);
+      $('.answer3').text(questionsArray.answer[2]);
+      $('.answer4').text(questionsArray.answer[3]);
+
+      
+        // you need to record the users choice and compare
+      
     }
   },
   stop: function() {
@@ -46,13 +84,14 @@ var stopwatch = {
     stopwatch.time--;
 
     var converted = stopwatch.timeConverter(stopwatch.time);
-    console.log(converted);
 
     // DONE: Use the variable we just created to show the converted time in the "display" div.
     $(".timer").text(converted);
     //this prevents the countdown going into the negative numbers. 
     if (stopwatch.time === 0 ) {
-      clearInterval(stopwatch)
+      // you have to do stopwatch.stop() is because object within a function. 
+      stopwatch.stop();
+      console.log("yes it works below 0")
     }
   },
   timeConverter: function(t) {
@@ -73,3 +112,14 @@ var stopwatch = {
     return minutes + ":" + seconds;
   }
 };
+
+
+
+//randomizes questions
+// function selectRandomQuestion(){
+//   var randomIndex = Math.floor(Math.random()*questionsArray.length);
+//   currentQuestion = questionsArray[randomIndex];
+
+//   // remove question from array.  
+//   questionsArray.splice(randomIndex,1);
+// }
