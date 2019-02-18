a0273ede2e74b0428b296b754525842eb17b3ae0
unknown <hampton.muriel@gmail.com>
2019-02-17 20:10:18 -0500

    game function mostly defined. Debugging why the question and answer still aren't displaying. and debuggin like 113 issue. it doesn't recognize or define [questionIndex]

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -12,55 +12,92 @@ window.onload = function () {
   $(".start").on("click", stopwatch.start);
 };
 
-
+var quesitonIndex = 0;
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
 
 questionsArray = [{
     question: "What does HTML mean?",
     answers: ["HyperText Markup Language", "Hypertech Markup Language", "Holly Toledo Make Loli", "Hyper Text Markup Language"],
     correctAnswer: "HyperText Markup Language",
+    // questionIndex: 0,
   },
   {
     question: "What symbol notates a class in CSS?",
     answers: ["@", "#", "%", "."],
     correctAnswer: ".",
+    // questionIndex: 1,
   },
   {
     question: "What does API stand represent?",
     answers: ["Anti-Pornography Initiative", "Application Programming Interface", "Application Properties Interface", "Applicaiton Proxy Interface"],
     correctAnswer: "Application Programming Interface",
+    // questionIndex: 2,
   }
 ];
-//  Variable that will hold our setInterval that runs the stopwatch
-var intervalId;
-
-// prevents the clock from being sped up unnecessarily
-var clockRunning = false;
 
 
 //game var's
 var playersGuess;
+
 //stuff hide() before game start 
 $('#submit').hide();
-//both buttons and divs should be hidden 
-$('.answer1').hide();
-$('.answer2').hide();
-$('.answer3').hide();
-$('.answer4').hide();
+$('.answers').hide();
+//changed it to 1 div class answer instead of one div for each answer. 
+
 
 // Our stopwatch object
-
 var stopwatch = {
   //change this back to 30secs after testing***
   // time: 30,
   time: 5,
-  // lap: 1,
 
   reset: function () {
-    stopwatch.time = 1000 * 30;
-    $(".timer").text("Timer: 00:30");
+    stopwatch.time = 1000 * 5;
+    $(".timer").text("Timer: 00:05");
     //change to 30
     this.time = 5;
     $('#submit').hide();
+    $('.answers').hide();
+  },
+
+  prepareNewQuestion: function () {
+    //This will empty out the results, add 1 to the quesitons index, and clear intervals
+    //before starting a new question
+    $('#results').empty();
+    questionIndex++;
+    //way better solution than messing with the time just wait5.. ugh!!! Learned this with Jonathan in a studygroup at Dom's. 
+    clearTimeout(wait5);
+    clearInterval(intervalId);
+    stopwatch.start();
+    stopwatch.time = 30;
+
+  },
+
+  verify: function () {
+    clockRunning = false;
+    //this stores the correct answer, and the selected answer in variables. 
+    var selectedAnswer = $('.selected').text();
+
+    var correctAnswer = questionsArray[quesitonIndex].correctAnswer;
+    //this will empty the question area and clear the answer radio buttons as well as the submit
+    $('#submit').hide();
+    var correctResult = $("<div>").text("The correct answer: " + correctAnswer);
+    correctAnswer.addClass("result");
+    var answerResult = $("<div>").text("Your answer: " + selectedAnswer);
+    answerResult.addClass("result");
+    $("#results").empty();
+    console.log("Correct answer: " + correctAnswer);
+    console.log("Correct answer: " + selectedAnswer);
+    $("#answers").empty();
+    $("#submit").hide();
+    $("#results").append(correctResult, answerResult);
+    wait5 = setTimeout(stopwatch.prepareNewQuestion, 1000 * 10);
+
+
+
   },
 
   start: function () {
@@ -69,19 +106,44 @@ var stopwatch = {
       intervalId = setInterval(stopwatch.count, 1000);
       clockRunning = true;
       $('#submit').show();
-      $('.answer1').show();
-      $('.answer2').show();
-      $('.answer3').show();
-      $('.answer4').show();
-      $('.questionArea').text(questionsArray[0].question);
-      //the answers aren't displaying 
-      $('.answer1').text(questionsArray[0].answers[0]);
-      $('.answer2').text(questionsArray[0].answers[1]);
-      $('.answer3').text(questionsArray[0].answers[2]);
-      $('.answer4').text(questionsArray[0].answers[3]);
+      $('#answer').show();
+      intervalId = setInterval(stopwatch.count, 1000);
+
+      //question area
+      var thisQuestion = questionsArray[questionIndex].question;
+      $('.questionArea').text(thisQuestion);
+
+      //answer area
+      //debuggin questionIndex isn't defined. I tried adding a quesitonIndex  property under questionArray.quesiton, but it caused line 113 to stop working. 
+      //I don't understand why line 113 works and can define [questionIndex] but a function within the object the same level can't
+      theseAnswers = questionArray[questionIndex].answers[i];
+      for (i = 0; i < theseAnswers.length; i++) {
+        //makes a new button and stores it
+        thisAnswer = $("<button>");
+        //This gives the new answer <button> a type and a class
+        thisAnswer.attr({
+          type: "radio",
+          class: "answer",
+        })
+        thisAnswer.text(theseAnswers[i]);
+        //with attaches the button to the multiple choice answer area'
+
+        $("#answers").append(thisAnswer);
+      };
+
+
+
     } else {
       alert("You've already clicked start. Pick an answer quick!");
     }
+    allAnswers - $('.answer');
+    allAnswers.on('click', function () {
+      //toggle means switch between the selected options. 
+      $(".selected").toggleClass("selected");
+      $(this).toggleClass("selected");
+    })
+    //makes sure the selected answer is verfied as the correctAnswer. 
+    $("#submit").on("click", stopwatch.verify);
   },
   //I've tried move this here and making it work outside the start counting fuction
   //it never works when I do. 
@@ -96,17 +158,18 @@ var stopwatch = {
   },
 
   count: function () {
-    stopwatch.time--;
-    var converted = stopwatch.timeConverter(stopwatch.time);
+    if (clockRunning === true) {
+      stopwatch.time--;
+      var converted = stopwatch.timeConverter(stopwatch.time);
 
-    // DONE: Use the variable we just created to show the converted time in the "display" div.
-    $(".timer").text(converted);
+      // DONE: Use the variable we just created to show the converted time in the "display" div.
+      $(".timer").text(converted);
 
-    //this prevents the countdown going into the negative numbers. 
-    if (stopwatch.time === 0) {
-      // you have to do stopwatch.stop() is because object within a function. 
-      stopwatch.stop();
-      console.log("yes it works below 0")
+      //this prevents the countdown going into the negative numbers. 
+      if (stopwatch.time === 0) {
+        // you have to do stopwatch.stop() is because object within a function. 
+        stopwatch.stop();
+      }
     }
   },
   //so I moved this from underneath the count()  so it would be separate and I could call it. 
